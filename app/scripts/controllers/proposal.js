1822aea5690b610071a2cd77dd3f5d31ffd56ab0
David Best <davidbest@Davids-MacBook-Pro-2.local>
2014-08-01 13:59:13 -0400

    Added tests for the ProposalCtrl and ProposalListCtrl. See 18F/afsmallbiz#74 Added basic tests for the ProposalCtrl and the ProposalListCtrl.

--- app/scripts/controllers/proposal.js
+++ app/scripts/controllers/proposal.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('sbirezApp')
-  .controller('ProposalCtrl', function ($scope, $http, $window, AuthenticationService) {
+  .controller('ProposalCtrl', function ($scope, $http, $window, $routeParams, AuthenticationService) {
     $scope.isLoggedIn = AuthenticationService.isLogged && $window.sessionStorage.token;
+    $scope.proposalId = $routeParams.documentId;
+    $scope.jwt = $window.sessionStorage.token;
+    $http.get('api/proposals/' + $scope.proposalId).success(function(data) {
+      $scope.data = data;
+    });
   });
