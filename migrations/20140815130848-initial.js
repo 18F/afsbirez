6c2fd4ea56ba0ade4f17cbe1777942febeca3111
Catherine Devlin <catherine.devlin@gmail.com>
2014-08-15 13:37:08 -0400

    New migration file (but still relying on .sync) See https://github.com/18F/afsmallbiz/issues/78

---
+++ migrations/20140815130848-initial.js
@@ -0,0 +1,18 @@
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    migration.createTable('user', {
+      id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+      name: {type: DataTypes.STRING, allowNull: false},
+      password: DataTypes.STRING,
+      }, {});
+    migration.createTable('file', {
+      id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+      metadata: DataTypes.STRING,
+      filepath: {type: DataTypes.STRING, allowNull: false},
+      }, {});            
+    done()
+  },
+  down: function(migration, DataTypes, done) {
+    migration.dropAllTables().complete(done);
+  }
+}
