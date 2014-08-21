6092d02746db2b5da25077e14a1a9550a83a4089
David Best <davidbest@Davids-MacBook-Pro-2.local>
2014-08-21 17:33:19 -0400

    Adding pagination for search. See 18F/afsmallbiz#77 Implemented pagination for the search control as a directive.

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -5,13 +5,17 @@ angular.module('sbirezApp')
     $scope.jwt = $window.sessionStorage.token;
     $scope.results = {};
 
-    var FBOPEN_URI = 'http://api.data.gov/gsa/fbopen-dev/v0/opps';
-    var FBOPEN_KEY = 'DEMO_KEY';
+    var FBOPEN_URI = 'http://api.data.gov/gsa/fbopen/v0/opps';
+    //var FBOPEN_KEY = 'DEMO_KEY';
+    //var FBOPEN_KEY = 'ftAQGUKHMf09pl9re18Vg1E3sEK5K1JQjPdpUiyG';
+    var FBOPEN_KEY = 'hd4gdCcFEms7xOkxkE7qv5N0Wde5kGggq0EI0NKT';
     var FBOPEN_SBIR = '(SBIR OR "small business innovation research" OR STTR OR "small business technology transfer")';
-    var SOLICITATIONS_PER_PAGE = 2;
+    var SOLICITATIONS_PER_PAGE = 10;
     $scope.simpleMode = true;
     $scope.searchTerm = '';
-
+    $scope.itemsPerPage = SOLICITATIONS_PER_PAGE;
+    $scope.currentPage = 0;
+    $scope.itemCount = 0;
     console.log('Search Ctrl');
 
     $scope.search = function(page) {
@@ -20,6 +24,14 @@ angular.module('sbirezApp')
       if (page === undefined) {
         page = 0;
       }
+      else if (page === 'next') {
+        page = $scope.currentPage + 1;
+      }
+      else if (page === 'prev') {
+        page = $scope.currentPage - 1;
+      }
+
+      $scope.currentPage = page;
 
       if ($scope.searchTerm !== '') {
         combinedSearch += ' AND ' + $scope.searchTerm;
@@ -29,12 +41,12 @@ angular.module('sbirezApp')
       config.params = [];
       config.params.q = combinedSearch;
       config.params.api_key = FBOPEN_KEY;
-      config.params.limit = SOLICITATIONS_PER_PAGE;
+//      config.params.limit = SOLICITATIONS_PER_PAGE;
       config.params.start = SOLICITATIONS_PER_PAGE * page;
       $http.get(FBOPEN_URI, config).success(function(data) {
         $scope.results = data;
+        $scope.itemCount = data.numFound;
         $scope.simpleMode = false;
-        console.log(data);
       });
     };
   });
---
+++ app/scripts/directives/pagination.js
@@ -0,0 +1,64 @@
+'use strict';
+
+angular.module('sbirezApp').directive('pagination', ['$compile', function($compile) {
+  return {
+    restrict: 'EA',
+    replace: false,
+    scope: {
+      'itemCount': '@',
+      'itemsPerPage': '@',
+      'method': '@',
+      'currentPage': '@'
+    },
+    link: function (scope, element, attrs) {
+      attrs.$observe('itemCount', function(value) {
+        updatePagination();
+      });
+      attrs.$observe('currentPage', function(value) {
+        updatePagination();
+      });
+
+      function updatePagination() {
+        var page = 0;
+        var linkTextStart = '<li';
+        var linkTextStart2 = '><a ng-href="#" ng-click="$parent.' + attrs.method + '(';
+        var linkTextMiddle = ')">';
+        var linkTextActive = ' class="active" ';
+        var linkTextEnd = '</a></li>';
+        var retVal = '<ul class="pagination">';
+        var currentItem = 0;
+        var currentPage = parseInt(scope.currentPage);
+        var itemsPerPage = parseInt(scope.itemsPerPage);
+        var itemCount = parseInt(scope.itemCount);
+
+        // add the left arrow, enabled if not at the first page
+        if (page !== currentPage) {
+          retVal += '<li><a href="#" ng-click="$parent.' + attrs.method + '(\'prev\')">&laquo;</a></li>'
+        }
+        else {
+          retVal += '<li class="disabled"><a href="#">&laquo;</a></li>'
+        }
+
+        // add the individual page callouts
+        for (currentItem; currentItem < itemCount; currentItem += itemsPerPage) {
+          retVal += linkTextStart + (currentPage === page ? linkTextActive : '') + linkTextStart2 + page + linkTextMiddle + (page+1) + linkTextEnd;
+          page++;
+        }
+
+        // add the right arrow, enabled if not at the last page
+        if (page - 1 !== currentPage) {
+          retVal += '<li><a href="#" ng-click="$parent.' + attrs.method + '(\'next\')">&raquo;</a></li>'
+        }
+        else {
+          retVal += '<li class="disabled"><a href="#">&raquo;</a></li>'
+        }
+
+        retVal += '</ul>';
+        element.html(retVal);
+        $compile(element.contents())(scope);
+      };
+
+      updatePagination();
+    }
+  };
+}]);
