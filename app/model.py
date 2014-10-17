de8675d0c7e374579d45dc851d4f234232f442ad
DaveCaraway <dave@fogmine.com>
2014-10-17 19:10:29 -0400

    See #91 generated models from the database

--- app/model.py
+++ app/model.py
@@ -1,53 +1,159 @@
-from flask.ext.sqlalchemy import SQLAlchemy
-# Define the database functions
-db = SQLAlchemy()
+# coding: utf-8
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, LargeBinary, String, Text, text
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
 
-    created_at = db.Column(db.DateTime, default=db.func.now())
-    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+Base = declarative_base()
+metadata = Base.metadata
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class Content(Base):
+    __tablename__ = 'contents'
 
-class Topic(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=db.func.now())
-    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    id = Column(Integer, primary_key=True, server_default=text("nextval('contents_id_seq'::regclass)"))
+    version = Column(Integer)
+    start_date = Column(DateTime(True))
+    end_date = Column(DateTime(True))
+    change_log = Column(Text)
+    content = Column(LargeBinary)
+    created_at = Column(DateTime(True), nullable=False)
+    updated_at = Column(DateTime(True), nullable=False)
+    document_id = Column(Integer)
 
-class Award(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=db.func.now())
-    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
-class Organization(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=db.func.now())
-    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+class Document(Base):
+    __tablename__ = 'documents'
 
-class Application(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=db.func.now())
-    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    id = Column(Integer, primary_key=True, server_default=text("nextval('documents_id_seq'::regclass)"))
+    name = Column(String(255))
+    description = Column(Text)
+    filepath = Column(String(255), nullable=False)
+    created_at = Column(DateTime(True), nullable=False)
+    updated_at = Column(DateTime(True), nullable=False)
+    organization_id = Column(ForeignKey(u'organizations.id', ondelete=u'RESTRICT', onupdate=u'CASCADE'))
 
-class Form(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=db.func.now())
-    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    organization = relationship(u'Organization')
 
-class Document(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=db.func.now())
-    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
-class Process(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=db.func.now())
-    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+class Documentskeyword(Base):
+    __tablename__ = 'documentskeywords'
+
+    created_at = Column(DateTime(True), nullable=False)
+    updated_at = Column(DateTime(True), nullable=False)
+    keyword_id = Column(ForeignKey(u'keywords.id'), primary_key=True, nullable=False)
+    document_id = Column(ForeignKey(u'documents.id'), primary_key=True, nullable=False)
+
+    document = relationship(u'Document')
+    keyword = relationship(u'Keyword')
+
+
+class Documentsproposal(Base):
+    __tablename__ = 'documentsproposals'
+
+    created_at = Column(DateTime(True), nullable=False)
+    updated_at = Column(DateTime(True), nullable=False)
+    proposal_id = Column(ForeignKey(u'proposals.id'), primary_key=True, nullable=False)
+    document_id = Column(ForeignKey(u'documents.id'), primary_key=True, nullable=False)
+
+    document = relationship(u'Document')
+    proposal = relationship(u'Proposal')
+
+
+class Keyword(Base):
+    __tablename__ = 'keywords'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('keywords_id_seq'::regclass)"))
+    keyword = Column(String(255))
+    created_at = Column(DateTime(True), nullable=False)
+    updated_at = Column(DateTime(True), nullable=False)
+
+
+class Organization(Base):
+    __tablename__ = 'organizations'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('organizations_id_seq'::regclass)"))
+    name = Column(String(255), nullable=False)
+    duns = Column(String(255))
+    ein = Column(String(255))
+    created_at = Column(DateTime(True), nullable=False)
+    updated_at = Column(DateTime(True), nullable=False)
+
+
+class Organizationsuser(Base):
+    __tablename__ = 'organizationsusers'
+
+    created_at = Column(DateTime(True), nullable=False)
+    updated_at = Column(DateTime(True), nullable=False)
+    organization_id = Column(ForeignKey(u'organizations.id'), primary_key=True, nullable=False)
+    user_id = Column(ForeignKey(u'users.id'), primary_key=True, nullable=False)
+
+    organization = relationship(u'Organization')
+    user = relationship(u'User')
+
+
+class Proposal(Base):
+    __tablename__ = 'proposals'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('proposals_id_seq'::regclass)"))
+    name = Column(String(255))
+    description = Column(Text)
+    sbir_topic_reference = Column(String(255))
+    start_date = Column(DateTime(True))
+    end_date = Column(DateTime(True))
+    organization_id = Column(ForeignKey(u'organizations.id', ondelete=u'RESTRICT', onupdate=u'CASCADE'))
+    workflow_id = Column(ForeignKey(u'workflows.id', ondelete=u'RESTRICT', onupdate=u'CASCADE'))
+    created_at = Column(DateTime(True), nullable=False)
+    updated_at = Column(DateTime(True), nullable=False)
+
+    organization = relationship(u'Organization')
+    workflow = relationship(u'Workflow')
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('users_id_seq'::regclass)"))
+    name = Column(String(255), nullable=False)
+    password = Column(String(255))
+    email = Column(String(255))
+    title = Column(String(255))
+    created_at = Column(DateTime(True), nullable=False)
+    updated_at = Column(DateTime(True), nullable=False)
+
+
+class Workflow(Base):
+    __tablename__ = 'workflows'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('workflows_id_seq'::regclass)"))
+    name = Column(String(255))
+    description = Column(Text)
+    created_at = Column(DateTime(True), nullable=False)
+    updated_at = Column(DateTime(True), nullable=False)
+
+
+class WorkflowStepResult(Base):
+    __tablename__ = 'workflowstepresults'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('workflowstepresults_id_seq'::regclass)"))
+    result = Column(Text)
+    completed_at = Column(DateTime(True))
+    workflowstep_id = Column(ForeignKey(u'workflowsteps.id', ondelete=u'RESTRICT', onupdate=u'CASCADE'))
+    created_at = Column(DateTime(True), nullable=False)
+    updated_at = Column(DateTime(True), nullable=False)
+
+    workflowstep = relationship(u'Workflowstep')
+
+
+class WorkflowStep(Base):
+    __tablename__ = 'workflowsteps'
+
+    id = Column(Integer, primary_key=True, server_default=text("nextval('workflowsteps_id_seq'::regclass)"))
+    name = Column(String(255))
+    description = Column(Text)
+    work = Column(Text)
+    workflow_id = Column(ForeignKey(u'workflows.id', ondelete=u'RESTRICT', onupdate=u'CASCADE'))
+    created_at = Column(DateTime(True), nullable=False)
+    updated_at = Column(DateTime(True), nullable=False)
+
+    workflow = relationship(u'Workflow')
