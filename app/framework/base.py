ebed1d36907768cdfc953a90cf8d99bdd532190f
DaveCaraway <dave@fogmine.com>
2014-11-25 11:34:47 -0500

    removed some instances of SBIR-EZ which were placed in invalid python locations

--- app/framework/base.py
+++ app/framework/base.py
@@ -11,14 +11,14 @@
 """
 
 
-class SBIR-EZError(Exception):
+class SBIREZError(Exception):
     """Base application error class."""
 
     def __init__(self, msg):
         self.msg = msg
 
 
-class SBIR-EZFormError(Exception):
+class SBIREZFormError(Exception):
     """Raise when an error processing a form occurs."""
 
     def __init__(self, errors=None):
