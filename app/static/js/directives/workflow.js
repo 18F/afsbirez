3ce0625101c7c8ba083a26cda8dfa7ad165fd12b
David Best <david.best@gsa.gov>
2014-12-01 13:06:38 -0500

    Adding a metromap view for sections.

--- app/static/js/directives/workflow.js
+++ app/static/js/directives/workflow.js
@@ -5,7 +5,8 @@ angular.module('sbirezApp').directive('workflow', function() {
     restrict: 'A',
     replace: true,
     scope: {
-      includeSidebar: '@'
+      includeSidebar: '@',
+      includeMetro: '@'
     },
     templateUrl: 'static/views/partials/workflow.html',
     controller: ['$scope', '$filter', '$window', '$location', 
@@ -17,11 +18,39 @@ angular.module('sbirezApp').directive('workflow', function() {
           description: 'This is a test workflow',
           sections: [
             {
-              name: "Main Section",
+              name: "Pre-Release",
+              id: "prerelease",
+              description: "This is the pre-release state.",
+              states: [
+                {
+                  id: 'prerelease-start',
+                  name: 'Information',
+                  description: 'This is the pre-release state.',
+                  nextState: 'released-start'
+                }
+              ]
+            },
+            {
+              name: "Released",
+              id: "released",
+              description: "This is the released state.",
+              states: [
+                {
+                  id: 'released-start',
+                  name: 'Information',
+                  description: 'This is the release state.',
+                  priorState: 'prerelease-start',
+                  nextState: 'proposals-start'
+                }
+              ]
+            },
+            {
+              name: "Proposals Accepted",
+              id: "proposalsaccepted",
               description: "This is the main section",
               states: [ 
                 {
-                  id: 'start',
+                  id: 'proposals-start',
                   name: 'First State',
                   description: 'This is the first state',
                   fields: [ 
@@ -40,6 +69,7 @@ angular.module('sbirezApp').directive('workflow', function() {
                       helpMessage: 'Email is required.'
                     }
                   ],
+                  priorState: 'released-start',
                   nextState: 'success1'
                 },
                 {
@@ -56,26 +86,97 @@ angular.module('sbirezApp').directive('workflow', function() {
                     }
                   ],
                   nextState: 'success2',
-                  priorState: 'start'
+                  priorState: 'proposals-start'
                 },
                 {
                   id: 'success2',
                   name: 'All Done',
                   description: 'Thank you for completing the workflow.',
                   priorState: 'success1',
-                  nextState: 'start2'
+                  nextState: 'techeval-start'
                 }
               ]
             },
             {
-              name: "Section 2",
-              description: "This is the second section.",
+              name: "Technical Evaluation",
+              id: "technicalevaluation",
+              description: "This is the technical evaluation section.",
               states: [
                 {
-                  id: 'start2',
-                  name: 'Start Next',
-                  description: 'Thank you for completing the workflow.',
-                  priorState: 'success2'
+                  id: 'techeval-start',
+                  name: 'Information',
+                  description: 'This is the technical evaluation.',
+                  priorState: 'success2',
+                  nextState: 'source-start'
+                }
+              ]
+            },
+            {
+              name: "Source Selection",
+              id: "sourceselection",
+              description: "This is the source selection section.",
+              states: [
+                {
+                  id: 'source-start',
+                  name: 'Information',
+                  description: 'This is the source selection section.',
+                  priorState: 'techeval-start',
+                  nextState: 'pr-start'
+                }
+              ]
+            },
+            {
+              name: "PR Package",
+              id: "prpackage",
+              description: "This is the PR Package section.",
+              states: [
+                {
+                  id: 'pr-start',
+                  name: 'Information',
+                  description: 'This is the PR Package section.',
+                  priorState: 'source-start',
+                  nextState: 'clarification-start'
+                }
+              ]
+            },
+            {
+              name: "Clarifications",
+              id: "clarifications",
+              description: "This is the clarifications section.",
+              states: [
+                {
+                  id: 'clarification-start',
+                  name: 'Information',
+                  description: 'This is the clarifications section.',
+                  priorState: 'pr-start',
+                  nextState: 'prepareaward-start'
+                }
+              ]
+            },
+            {
+              name: "Prepare Award",
+              id: "prepareaward",
+              description: "This is the prepare award section.",
+              states: [
+                {
+                  id: 'prepareaward-start',
+                  name: 'Information',
+                  description: 'This is the prepare award section.',
+                  priorState: 'clarification-start',
+                  nextState: 'award-start'
+                }
+              ]
+            },
+            {
+              name: "Award",
+              id: "award",
+              description: "This is the award section.",
+              states: [
+                {
+                  id: 'award-start',
+                  name: 'Information',
+                  description: 'This is the award section.',
+                  priorState: 'prepareaward-start'
                 }
               ]
             }
@@ -90,6 +191,14 @@ angular.module('sbirezApp').directive('workflow', function() {
           }
         };
 
+        var getSectionIdByStateId = function(id) {
+          for (var i = 0; i < $scope.workflow.sections.length; i++) {
+            for (var j = 0; j < $scope.workflow.sections[i].states.length; j++) {
+              if ($scope.workflow.sections[i].states[j].id === id) return $scope.workflow.sections[i].id;
+            }
+          }
+        };
+
         var getStateDataById = function(id) {
           for (var i = 0; i < $scope.stateData.length; i++) {
             if ($scope.stateData[i].id === id) return $scope.stateData[i];
@@ -100,11 +209,12 @@ angular.module('sbirezApp').directive('workflow', function() {
         };
 
         $scope.currentStateIndex = 1;
-        $scope.currentState = getStateById('start');
-        $scope.currentStateData = getStateDataById('start');
+        $scope.currentState = $scope.workflow.sections[0].states[0];
+        $scope.currentStateData = getStateDataById($scope.currentState.id);
+        $scope.currentSectionId = $scope.workflow.sections[0].id;
 
         $scope.showBackButton = function() {
-          return $scope.currentState.id !== 'start';
+          return $scope.currentState.id !== $scope.workflow.sections[0].states[0].id; 
         };
 
         $scope.showNextButton = function() {
@@ -121,6 +231,7 @@ angular.module('sbirezApp').directive('workflow', function() {
 
         $scope.backState = function() {
           $scope.currentState = getStateById($scope.currentState.priorState);
+          $scope.currentSectionId = getSectionIdByStateId($scope.currentState.id);
           $scope.currentStateIndex--;
         };
 
@@ -144,6 +255,7 @@ angular.module('sbirezApp').directive('workflow', function() {
         $scope.nextState = function() {
           if (validate()) {
             $scope.currentState = getStateById($scope.currentState.nextState);
+            $scope.currentSectionId = getSectionIdByStateId($scope.currentState.id);
             $scope.currentStateIndex++;
           }
         };
@@ -157,14 +269,19 @@ angular.module('sbirezApp').directive('workflow', function() {
                 if (state === $scope.workflow.sections[i].states[j].id) {
                   $scope.currentState = $scope.workflow.sections[i].states[j];
                   $scope.currentStateIndex = count;
+                  $scope.currentSectionId = $scope.workflow.sections[i].id;
                 }
               }
             }
           }
         };
 
-        $scope.isCurrentId = function(state) {
+        $scope.isCurrentState = function(state) {
           return state === $scope.currentState.id;
+        };
+
+        $scope.isCurrentSection = function(section) {
+          return section === $scope.currentSectionId;
         };
       }
     ]
