1b8c9809c68ba0f36a74ef77700093e998b52d29
Annalee Flower Horne <leeflowerh@gmail.com>
2015-04-03 16:35:24 -0400

    migration merge.

---
+++ sbirez/migrations/0018_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('sbirez', '0015_auto_20150326_1735'),
+        ('sbirez', '0017_auto_20150403_1944'),
+    ]
+
+    operations = [
+    ]
