5bf2d952b51f0ad1709be15add40e5d96d22b2f5
David Best <david.best@gsa.gov>
2015-06-29 21:49:22 -0400

    Adding jargon support to the frontend.
    * Added bigfoot to the build.
    * Added jargon directive to instantiate footnotes.
    * Added a first pass of jargon definitions to the workflow.

---
+++ sbirez/static/js/directives/jargon.js
@@ -0,0 +1,39 @@
+'use strict';
+
+angular.module('sbirezApp').directive('jargon', function(){
+    return {
+      restrict: 'AE',
+      scope: {
+        jargon: '='
+      },
+      link: function (scope, element, attrs) {
+
+        function spliceSlice(str, index, count, add) {
+          return str.slice(0, index) + (add || '') + str.slice(index + count);
+        }
+
+        function set(val) {
+          var startIndex, endIndex;
+          startIndex = val.indexOf('<jargon');
+          while (startIndex !== -1) {
+            endIndex = val.indexOf('>',startIndex);
+            var id = val.substr(startIndex + 8, endIndex - startIndex - 8);
+            for (var i = 0; i < scope.jargon.jargons.length; i++) {
+              if (scope.jargon.jargons[i].name === id) {
+                val = spliceSlice(val, startIndex, endIndex - startIndex + 1, 
+                  '<a id="fn-' + i + '-a" href="#fn-' + i + '" class="footnote-button" rel="footnote">' + i + '</a>' + 
+                  '<section class="footnotes"><ol><li class="footnote" id="fn-' + i + '">' + 
+                  scope.jargon.jargons[i].html + 
+                  '<a href="#fn-' + i + '-a" class="reversefootnote">R</a></li></ol></section>');
+                break;
+              }
+            }
+            startIndex = val.indexOf('<jargon');
+          }
+          element.html(val);
+        }
+
+        set(scope.jargon.human || element.text() || '');
+      }
+    };
+});
