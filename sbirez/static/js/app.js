5dd45cc6f395736767baeb65c0ce437897d692ce
David Best <david.best@gsa.gov>
2015-08-12 14:33:41 -0400

    Disabling the pre-production flag.

--- sbirez/static/js/app.js
+++ sbirez/static/js/app.js
@@ -236,7 +236,7 @@ angular.module('sbirezApp', [
   })
 
 .run(function($rootScope, $location, $state, $window, AuthenticationService) {
-  $rootScope.preproduction = true;
+  $rootScope.preproduction = false;
   $rootScope.$on('$stateChangeError',function(event, toState, toParams, fromState, fromParams){
     //console.log('$stateChangeError - fired when an error occurs during transition.');
     //console.log(arguments);
