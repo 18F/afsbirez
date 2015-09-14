c638ec7d70f907820e5432502d374e2eb7a714b1
David Best <david.best@gsa.gov>
2015-09-14 12:17:11 -0400

    Fixing dumb merge conflict & adding a merge migration.

---
+++ sbirez/migrations/0034_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('sbirez', '0033_auto_20150910_2059'),
+        ('sbirez', '0033_merge'),
+    ]
+
+    operations = [
+    ]
