1fe4dd07142190bc491d306436022580171ec1e0
David Best <david.best@gsa.gov>
2015-09-21 08:58:39 -0400

    Re-adding the shell out to psql to check the name of the solicitations, but adding a condition to keep it from stopping the process if the command is not found.

--- sbirez/management/commands/importtopics.py
+++ sbirez/management/commands/importtopics.py
@@ -26,16 +26,24 @@ class Command(BaseCommand):
         if not os.path.isfile(mdb_filename):
             raise OSError("MS Access database %s not found" % mdb_filename)
 
+        not_found = False
+
         # Confirm that solicitation_name is for solicitation in the database
-        #proc = subprocess.Popen(#'ls',
-        #                        'psql -t -c "SELECT name FROM sbirez_solicitation" %s' %
-        #                        settings.DATABASES['default']['NAME'],
-        #                        stdout=subprocess.PIPE, shell=True)
-        #solicitation_names = [s.decode("utf-8").strip()
-        #                      for s in proc.stdout.read().splitlines()
-        #                      if s.strip()]
-        #if solicitation_name not in solicitation_names:
-        #    raise CommandError("Available solicitation_names are: " + ", ".join(solicitation_names))
+        proc = subprocess.Popen(#'ls',
+                                'psql -t -c "SELECT name FROM sbirez_solicitation" %s' %
+                                settings.DATABASES['default']['NAME'],
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, 
+                                shell=True)
+        error_status = proc.stderr.read().decode("utf-8").strip()
+        if "command not found" in error_status:
+            not_found = True
+            print("WARNING: Unable to find psql command.")
+
+        solicitation_names = [s.decode("utf-8").strip()
+                              for s in proc.stdout.read().splitlines()
+                              if s.strip()]
+        if not not_found and solicitation_name not in solicitation_names:
+            raise CommandError("Available solicitation_names are: " + ", ".join(solicitation_names))
 
         # Use MDB tools to dump CSVs from MS Access .mdb file
         os.system("mdb-export %s commands > data/command.csv" % mdb_filename)
@@ -47,6 +55,5 @@ class Command(BaseCommand):
         os.system("""psql -v ON_ERROR_STOP=1 -v solicitation="'%s'" -f data/copy_topic_csvs.sql %s""" %
                   (solicitation_name, settings.DATABASES['default']['NAME']))
                   '''
-
 
         call_command('indextopics')
